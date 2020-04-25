e7a483d6ccb001f2dbd45b484212fba1cdb8f632
caatclaudia <a21270617@isec.pt>
2020-04-25 22:11:17 +0100

    Mapa sai com 'fim'

--- SOTaxi/Mapa/Source.cpp
+++ SOTaxi/Mapa/Source.cpp
@@ -7,29 +7,47 @@
 #include <io.h>
 
 #define TAM 200
+#define EVENT_ATUALIZAMAP TEXT("AtualizaMap")
+#define NOME_MUTEXMAPA TEXT("MutexMapa")
 
 typedef struct {
 	TCHAR caracter;
 } MAPA;
 
+typedef struct {
+	MAPA mapa[TAM][TAM];
+	TCHAR* pView;
+	int terminar;
+} DADOS;
+
 #define PATH TEXT("..\\mapa.txt")
 
 HANDLE hFile, map;
-TCHAR* pView;
-MAPA mapa[TAM][TAM];
+HANDLE hMutex;
 
-void leFicheiro();
-void mostraMapa();
+DWORD WINAPI ThreadPrincipal(LPVOID param);
+DWORD WINAPI ThreadSair(LPVOID param);
+void leFicheiro(DADOS* dados);
+void mostraMapa(DADOS* dados);
 
 int _tmain(int argc, TCHAR argv[]) {
-	
+	HANDLE hThreadSair, hThreadPrincipal;
+	DADOS dados;
 	TCHAR nome[100] = PATH;
 	int x = 0, y = 0;
+	dados.terminar = 0;
 
 #ifdef UNICODE
 	_setmode(_fileno(stdin), _O_WTEXT);
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif
+
+	hMutex = CreateMutex(NULL, FALSE, NOME_MUTEXMAPA);
+	if (hMutex == NULL) {
+		_tprintf(TEXT("\n[ERRO] Erro ao criar Mutex!\n"));
+		return -1;
+	}
+	WaitForSingleObject(hMutex, INFINITE);
 
 	hFile = CreateFile(PATH, GENERIC_WRITE | GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
@@ -48,28 +66,40 @@ int _tmain(int argc, TCHAR argv[]) {
 		return -1;
 	}
 
-	leFicheiro();
+	leFicheiro(&dados);
+	ReleaseMutex(hMutex);
 
-	while (1) {
-		mostraMapa();
-		
-		system("cls");
-		Sleep(3000);
+	hThreadPrincipal = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadPrincipal, (LPVOID)&dados, 0, NULL); //CREATE_SUSPENDED para nao comecar logo
+	if (hThreadPrincipal == NULL) {
+		_tprintf(TEXT("\n[ERRO] Erro ao lançar Thread!\n"));
+		return 0;
 	}
+
+	hThreadSair = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadSair, (LPVOID)&dados, 0, NULL); //CREATE_SUSPENDED para nao comecar logo
+	if (hThreadSair == NULL) {
+		_tprintf(TEXT("\n[ERRO] Erro ao lançar Thread!\n"));
+		return 0;
+	}
+
+	HANDLE ghEvents[2];
+	ghEvents[0] = hThreadPrincipal;
+	ghEvents[1] = hThreadSair;
+	WaitForMultipleObjects(2, ghEvents, TRUE, INFINITE);
+	
 
 	_tprintf(TEXT("\nPrima uma tecla...\n"));
 	_gettch();
 
-	UnmapViewOfFile(pView);
+	UnmapViewOfFile(dados.pView);
 	CloseHandle(hFile);
 	CloseHandle(map);
 
 	return 0;
 }
 
-void leFicheiro() {
-	pView = (TCHAR*)MapViewOfFile(map, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 50 * 52);
-	if (pView == NULL)
+void leFicheiro(DADOS *dados) {
+	dados->pView = (TCHAR*)MapViewOfFile(map, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 50 * 52);
+	if (dados->pView == NULL)
 	{
 		_tprintf(TEXT("\n[ERRO] Erro em MapViewOfFile!\n"));
 		CloseHandle(hFile);
@@ -80,7 +110,7 @@ void leFicheiro() {
 	TCHAR aux;
 	int x=0, y = 0;
 	for (int i = 0; i < 50 * 52; i++) {
-		aux = pView[i];
+		aux = dados->pView[i];
 		_tprintf(TEXT("%c"), aux);
 		if (aux == '\n') {
 			x = 0;
@@ -90,19 +120,54 @@ void leFicheiro() {
 			x++;
 		}
 		if (aux != '\n') {
-			mapa[y][x].caracter = aux;
+			dados->mapa[y][x].caracter = aux;
 		}
 	}
 
 	return;
 }
 
-void mostraMapa(){
+void mostraMapa(DADOS* dados){
 	for (int x = 0; x < 50; x++) {
 		for (int y = 0; y < 52; y++)
-			_tprintf(TEXT("%c"), mapa[x][y].caracter);
+			_tprintf(TEXT("%c"), dados->mapa[x][y].caracter);
 		_tprintf(TEXT("\n"));
 	}
 
 	return;
+}
+
+DWORD WINAPI ThreadPrincipal(LPVOID param) {
+	DADOS* dados = ((DADOS*)param);
+
+	while (!dados->terminar) {
+		WaitForSingleObject(hMutex, INFINITE);
+		mostraMapa(dados);
+		system("cls");
+		ReleaseMutex(hMutex);
+		
+		Sleep(3000);
+	}
+
+	Sleep(500);
+
+	ExitThread(0);
+}
+
+DWORD WINAPI ThreadSair(LPVOID param) {
+	TCHAR op[TAM];
+	DADOS* dados = ((DADOS*)param);
+
+	do {
+		_fgetts(op, TAM, stdin);
+		op[_tcslen(op) - 1] = '\0';
+		WaitForSingleObject(hMutex, INFINITE);
+	} while (_tcscmp(op, TEXT("fim")));
+
+	dados->terminar = 1;
+	ReleaseMutex(hMutex);
+
+	Sleep(1000);
+
+	ExitThread(0);
 }
