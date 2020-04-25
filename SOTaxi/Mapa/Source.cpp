953153da59ffce01a4e8850fbdcb5d352aeaeff3
caatclaudia <a21270617@isec.pt>
2020-04-25 19:09:11 +0100

    Mapa em Array[200][200]
    Não esta dinamico

--- SOTaxi/Mapa/Source.cpp
+++ SOTaxi/Mapa/Source.cpp
@@ -6,17 +6,25 @@
 #include <conio.h>
 #include <io.h>
 
+#define TAM 200
+
 typedef struct {
 	TCHAR caracter;
 } MAPA;
 
 #define PATH TEXT("..\\mapa.txt")
 
+HANDLE hFile, map;
+TCHAR* pView;
+MAPA mapa[TAM][TAM];
+
+void leFicheiro();
+void mostraMapa();
+
 int _tmain(int argc, TCHAR argv[]) {
-	HANDLE hFile, map;
-	MAPA* pView;
-	TCHAR aux;
+	
 	TCHAR nome[100] = PATH;
+	int x = 0, y = 0;
 
 #ifdef UNICODE
 	_setmode(_fileno(stdin), _O_WTEXT);
@@ -40,22 +48,13 @@ int _tmain(int argc, TCHAR argv[]) {
 		return -1;
 	}
 
-	pView = (MAPA*)MapViewOfFile(map, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 50 * 52);
-	if (pView == NULL)
-	{
-		_tprintf(TEXT("\n[ERRO] Erro em MapViewOfFile!\n"));
-		CloseHandle(hFile);
-		CloseHandle(map);
-		return -1;
-	}
+	leFicheiro();
 
 	while (1) {
-		for (int i = 0; i < 50 * 52; i++) {
-			aux = pView[i].caracter;
-			_tprintf(TEXT("%c"), aux);
-		}
+		mostraMapa();
+		
 		system("cls");
-		Sleep(1000);
+		Sleep(3000);
 	}
 
 	_tprintf(TEXT("\nPrima uma tecla...\n"));
@@ -66,4 +65,44 @@ int _tmain(int argc, TCHAR argv[]) {
 	CloseHandle(map);
 
 	return 0;
+}
+
+void leFicheiro() {
+	pView = (TCHAR*)MapViewOfFile(map, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 50 * 52);
+	if (pView == NULL)
+	{
+		_tprintf(TEXT("\n[ERRO] Erro em MapViewOfFile!\n"));
+		CloseHandle(hFile);
+		CloseHandle(map);
+		return;
+	}
+
+	TCHAR aux;
+	int x=0, y = 0;
+	for (int i = 0; i < 50 * 52; i++) {
+		aux = pView[i];
+		_tprintf(TEXT("%c"), aux);
+		if (aux == '\n') {
+			x = 0;
+			y++;
+		}
+		else {
+			x++;
+		}
+		if (aux != '\n') {
+			mapa[y][x].caracter = aux;
+		}
+	}
+
+	return;
+}
+
+void mostraMapa(){
+	for (int x = 0; x < 50; x++) {
+		for (int y = 0; y < 52; y++)
+			_tprintf(TEXT("%c"), mapa[x][y].caracter);
+		_tprintf(TEXT("\n"));
+	}
+
+	return;
 }
