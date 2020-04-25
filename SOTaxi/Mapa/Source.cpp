784f57285645b0e9bdafb796d0df239043ebf959
caatclaudia <a21270617@isec.pt>
2020-04-25 16:16:36 +0100

    Leitura de Mapa
    Não está a ler de forma dinamica nem a armazenar numa estrutura, mas sim num TCHAR*

---
+++ SOTaxi/Mapa/Source.cpp
@@ -0,0 +1,65 @@
+#include <Windows.h>
+#include <tchar.h>
+#include <math.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <conio.h>
+#include <io.h>
+
+typedef struct {
+	TCHAR caracter;
+} MAPA;
+
+#define PATH TEXT("..\\mapa.txt")
+
+int _tmain(int argc, TCHAR argv[]) {
+	HANDLE hFile, map;
+	MAPA* pView;
+	TCHAR aux;
+	TCHAR nome[100] = PATH;
+
+#ifdef UNICODE
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+#endif
+
+	hFile = CreateFile(PATH, GENERIC_WRITE | GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		_tprintf(TEXT("Terminal failure: Unable to open file.\n"));
+		return -1;
+	}
+
+	map = CreateFileMapping(hFile, NULL, PAGE_READWRITE, 0, 50 * 52, NULL);
+
+	if (map == NULL)
+	{
+		_tprintf(TEXT("Terminal failure: Unable to create file mapping.\n"));
+		CloseHandle(hFile);
+		return -1;
+	}
+
+	pView = (MAPA*)MapViewOfFile(map, FILE_MAP_READ | FILE_MAP_WRITE, 0, 0, 50 * 52);
+	if (pView == NULL)
+	{
+		_tprintf(TEXT("Terminal failure: MapViewOfFile.\n"));
+		CloseHandle(hFile);
+		CloseHandle(map);
+		return -1;
+	}
+
+	for (int i = 0; i < 50 * 52; i++) {
+		aux = pView[i].caracter;
+		_tprintf(TEXT("%c"), aux);
+	}
+
+	_tprintf(TEXT("\n\nCarregue numa tecla!\n"));
+	_gettch();
+
+	UnmapViewOfFile(pView);
+	CloseHandle(hFile);
+	CloseHandle(map);
+
+	return 0;
+}
