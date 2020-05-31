8abc331dc50494c35c40cba28f99d8a638345991
caatclaudia <a21270617@isec.pt>
2020-05-31 19:45:26 +0100

    Distinguir Taxi de Passageiros
    Através de Bitmap

--- SOTaxi/MapInfo/MapInfo.cpp
+++ SOTaxi/MapInfo/MapInfo.cpp
@@ -2,6 +2,7 @@
 #include <tchar.h>
 #include <windowsx.h>
 #include "MapInfo.h"
+#include "resource.h"
 
 HANDLE hThreadAtualizaMapa;
 DADOS dados;
@@ -12,6 +13,13 @@ void(*ptr_register)(TCHAR*, int);
 LRESULT CALLBACK TrataEventos(HWND, UINT, WPARAM, LPARAM);
 
 TCHAR szProgName[] = TEXT("MapInfo");
+
+//Icons
+HBITMAP hTaxiLivre;
+HBITMAP hTaxiOcupado;
+HBITMAP hPessoaSemTaxi;
+HBITMAP hPessoaComTaxi;
+HDC hdcTaxiLivre, hdcTaxiOcupado, hdcPessoaSemTaxi, hdcPessoaComTaxi;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow) {
 	dados.terminar = 0;
@@ -128,6 +136,7 @@ LRESULT CALLBACK TrataEventos(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lPara
 	HFONT hFont;
 	TCHAR caract;
 	int xPos = 0, yPos = 0;
+	int mWith, mHeight;
 
 	hFont = CreateFont(12, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
 		CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH, TEXT("Impact"));
@@ -185,7 +194,44 @@ LRESULT CALLBACK TrataEventos(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lPara
 			caract = shared[i].caracter;
 			rect.left = 80 + (8 * xPos);
 			rect.top = 15 + (9 * yPos);
-			DrawText(hdc, &caract, 1, &rect, DT_SINGLELINE | DT_NOCLIP);
+			if(caract=='_' || caract=='X')
+				DrawText(hdc, &caract, 1, &rect, DT_SINGLELINE | DT_NOCLIP);
+			else {
+				int x = (xPos - 80) / 8;
+				int y = (yPos - 15) / 9;
+				if (isdigit(caract)) {
+					for (int i = 0; i < info.ntaxis; i++) {
+						if (info.taxis[i].X == x && info.taxis[i].Y == y && info.taxis[i].disponivel == 1) {
+							hdcTaxiLivre = CreateCompatibleDC(hdc);
+							hTaxiLivre = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BITMAP1));
+							SelectObject(hdcTaxiLivre, hTaxiLivre);
+							BitBlt(hdc, rect.left, rect.top, 100, 100, hdcTaxiLivre, 0, 0, SRCCOPY);
+						}
+						else if (info.taxis[i].X == x && info.taxis[i].Y == y) {
+							hdcTaxiOcupado = CreateCompatibleDC(hdc);
+							hTaxiOcupado = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BITMAP2));
+							SelectObject(hdcTaxiOcupado, hTaxiOcupado);
+							BitBlt(hdc, rect.left, rect.top, 100, 100, hdcTaxiOcupado, 0, 0, SRCCOPY);
+						}
+					}
+				}
+				else {
+					for (int i = 0; i < info.npassageiros; i++) {
+						if (info.passageiros[i].X == x && info.passageiros[i].Y == y && info.passageiros[i].tempoEspera == -1) {
+							hdcPessoaSemTaxi = CreateCompatibleDC(hdc);
+							hPessoaSemTaxi = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BITMAP3));
+							SelectObject(hdcPessoaSemTaxi, hPessoaSemTaxi);
+							BitBlt(hdc, rect.left, rect.top, 100, 100, hdcPessoaSemTaxi, 0, 0, SRCCOPY);
+						}
+						else if (info.passageiros[i].X == x && info.passageiros[i].Y == y) {
+							hdcPessoaComTaxi = CreateCompatibleDC(hdc);
+							hPessoaComTaxi = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BITMAP4));
+							SelectObject(hdcPessoaComTaxi, hPessoaComTaxi);
+							BitBlt(hdc, rect.left, rect.top, 100, 100, hdcPessoaComTaxi, 0, 0, SRCCOPY);
+						}
+					}
+				}
+			}
 			xPos++;
 			if (xPos == (tamanhoMapa + 1)) {
 				yPos++;
